3de9102aeda3d58d609f4e263daa815a0415fd8c
mspar123 <mohammadparwani@outlook.com>
2026-09-03 14:36:19 +0200

    Quiz api geïmplementeerd en projectstructuur opgeschoond.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/questions': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/checkanswers': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 })
